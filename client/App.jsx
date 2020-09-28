72ed1d010513dfb424e8eda98c30c064cc29a472
Lars Nyström <nystrom.lasse@gmail.com>
2020-09-28 22:15:03 +0200

    Add answers content

--- client/App.jsx
+++ client/App.jsx
@@ -7,6 +7,15 @@ export const App = ({ questions, answers }) => (
         {questions.map(({questionId, content}) =>(
             <div key={questionId}>
                 <h3>{content}</h3>
+                <div>
+                    {answers.filter(answer => answer.questionId === questionId).map(
+                        ({ content, upvotes, answerId }) => (
+                            <div key={answerId}>
+                                <span>{content} - {upvotes}</span>
+                            </div>
+                        )
+                    )}
+                </div>
             </div>
         ))}
     </div>
